d6d5247ae159d2227eb88f0bdb4cf1cf33bea302
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2024-10-17 01:05:42 +0300

    basic movement

---
+++ scripts/camera.gd
@@ -0,0 +1,45 @@
+extends Node3D
+
+signal set_cam_rotation(_cam_rotation : float)
+@export var player : CharacterBody3D
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var spring_arm = $CamYaw/CamPitch/SpringArm3D
+@onready var camera = $CamYaw/CamPitch/SpringArm3D/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+var pitch_max : float = 75
+var pitch_min : float = -55
+var position_offset : Vector3 = Vector3(0, 1, 0)
+var position_offset_target : Vector3 = Vector3(0, 1, 0)
+
+var is_aiming := false
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	spring_arm.add_excluded_object(player.get_rid())
+	#top_level = true
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += -event.relative.y * pitch_sensitivity
+func _process(delta: float) -> void:
+	var view = Input.get_vector("view_left", "view_right", "view_down", "view_up")
+	yaw += -view.x * yaw_sensitivity * 5.0
+	pitch += view.y * pitch_sensitivity * 5.0
+
+func _physics_process(delta):
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	
+	#if you don't want to lerp, set them directly
+	#yaw_node.rotation_degrees.y = yaw
+	#pitch_node.rotation_degrees.x = pitch
+	
---
+++ scripts/player.gd
@@ -0,0 +1,62 @@
+extends CharacterBody3D
+
+@onready var model : Node3D = $Model
+@onready var animation_tree : AnimationTree = $Model/Sophia/AnimationTree
+@onready var animation_player: AnimationPlayer = $Model/Sophia/AnimationPlayer
+
+#@onready var planet = $/root/World2/Planet
+
+var jump_buffer := 0.0
+const ROTATION_SPEED := 10.0
+const JUMP_VELOCITY := 20.0
+const PUSH_FORCE := 1.0
+
+func _physics_process(delta: float) -> void:
+	#var d = global_position - planet.global_position
+	#var gravity_force = (-d.normalized()).normalized()
+	var gravity_force = Vector3(0,-1,0)
+	jump_buffer = clamp((jump_buffer - delta * 10.0), 0.0, 50.0)
+	velocity = gravity_force * 10.0 + global_transform.basis.y * jump_buffer
+	
+	if jump_buffer <= 10.0:
+		animation_tree["parameters/conditions/fall"] = true
+	
+	var up_direction = -gravity_force.normalized()
+	var orientation_direction = Quaternion(global_transform.basis.y, up_direction) * global_transform.basis.get_rotation_quaternion()
+	var rotation = global_transform.basis.get_rotation_quaternion().slerp(orientation_direction.normalized(), 5.0 * delta)
+	global_rotation = rotation.get_euler()
+	
+	
+	var movement := Vector3.ZERO
+	var forward : Vector3 = -$CamRoot/CamYaw.global_transform.basis.z
+	var right : Vector3 = $CamRoot/CamYaw.global_transform.basis.x
+	var up : Vector3 = global_transform.basis.y
+	var direction = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
+	forward *= direction.y
+	right *= direction.x
+	movement += forward + right
+	if movement != Vector3.ZERO:
+		if jump_buffer == 0.0: animation_tree["parameters/conditions/run"] = true
+		$Look/Point.position = Vector3(direction.x, 0, -direction.y)
+		$Model/Sophia.rotation.y = lerp_angle($Model/Sophia.rotation.y, $CamRoot/CamYaw.rotation.y + deg_to_rad(180), ROTATION_SPEED * delta)
+		var model_transform = model.transform.interpolate_with(model.transform.looking_at($Look/Point.position), ROTATION_SPEED * delta)
+		model.transform = model_transform
+
+		velocity += movement.normalized() * 5.0
+	elif jump_buffer == 0.0: animation_tree["parameters/conditions/idle"] = true
+	
+	move_and_slide()
+	apply_impulse()
+	
+func apply_impulse() -> void:
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider() is RigidBody3D:
+			collision.get_collider().apply_central_impulse(-collision.get_normal() * PUSH_FORCE)
+
+func _input(event) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("jump") and jump_buffer == 0.0 and animation_tree["parameters/playback"].get_fading_from_node() == "": 
+		jump_buffer = JUMP_VELOCITY
+		animation_tree["parameters/conditions/jump"] = true
