cef95738b8b1739f3bc0c1a4ed7735f0aac48ee2
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2024-10-25 01:54:38 +0300

    ball grow, shrink

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,1 +1,17 @@
 extends RigidBody3D
+
+func _process(delta: float) -> void:
+	#grow(0.5 * delta)
+	pass
+
+func grow(value:float) -> void:
+	$Mesh.scale += Vector3(value, value, value)
+	$Collision.scale += Vector3(value, value, value)
+func shrink(value:float) -> void:
+	$Mesh.scale -= Vector3(value, value, value)
+	$Collision.scale -= Vector3(value, value, value)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_1): shrink(0.5)
+	elif Input.is_key_pressed(KEY_2): grow(0.5)
