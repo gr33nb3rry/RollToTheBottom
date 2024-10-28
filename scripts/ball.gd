ddce264ced1b251aabe81e54e9d404b252c90cd2
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2024-10-28 02:28:19 +0200

    materials

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,17 +2,18 @@ extends RigidBody3D
 
 const INITIAL_MASS : float = 25.0
 
-
 func grow(value:float) -> void:
 	$Mesh.scale += Vector3(value, value, value)
 	$Collision.scale += Vector3(value, value, value)
 	$Area/Collision.scale += Vector3(value, value, value)
 	mass += clamp(INITIAL_MASS * value, 1.0, 75.0)
+	update_pieces()
 func shrink(value:float) -> void:
 	$Mesh.scale -= Vector3(value, value, value)
 	$Collision.scale -= Vector3(value, value, value)
 	$Area/Collision.scale -= Vector3(value, value, value)
 	mass -= clamp(INITIAL_MASS * value, 1.0, 75.0)
+	update_pieces()
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_1): shrink(0.05)
@@ -33,6 +34,11 @@ func _on_area_area_entered(area: Area3D) -> void:
 
 func _on_area_area_exited(area: Area3D) -> void:
 	pass
+	
+func update_pieces() -> void:
+	for p in $Pieces.get_children():
+		var direction = (p.position - Vector3.ZERO).normalized()
+		p.position = direction * 2.0 * $Mesh.scale.x
 
 func get_volume(r:float):
 	return (4.0/3.0)*PI*pow(r,3)
